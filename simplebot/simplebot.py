2afafba0b2c2f16788ab733a4f446253518a6efe
27Saumya <64534496+27Saumya@users.noreply.github.com>
2022-03-30 08:48:11 +0530

    Change `simplebot/cogs` -> `./cogs`

--- simplebot/simplebot.py
+++ simplebot/simplebot.py
@@ -10,7 +10,7 @@ def run(discord_token):
     """ Create the bot, add the cogs and run it. """
     bot = commands.Bot(command_prefix=('s!'), case_insensitive=True, debug_guilds=[881207955029110855], intents=discord.Intents.all())
 
-    for filename in os.listdir('simplebot/cogs'):
+    for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
 
