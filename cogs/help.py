ba5213e09b108e70c290b5ef7f300ba4322d35b7
ARCIEL044 <92455694+ARCIEL044@users.noreply.github.com>
2022-05-05 05:49:18 +0800

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,1 +1,79 @@
+import traceback
+import sys
+import discord
+from discord.ext import commands
+import Levenshtein as lev
 
+class HelpCog(commands.Cog):
+    """ Handles everything related to the help menu. """
+
+    def __init__(self, bot):
+        """ Set attributes and remove default help command. """
+        self.bot = bot
+        self.bot.remove_command('help')
+
+    def help_embed(self, title):
+        embed = discord.Embed(title=title)
+        prefix = self.bot.command_prefix
+        prefix = prefix[0] if prefix is not str else prefix
+
+        for cog in self.bot.cogs:  # Uset bot.cogs instead of bot.commands to control ordering in the help embed
+            for cmd in self.bot.get_cog(cog).get_commands():
+                if cmd.brief:
+                    if cmd.usage:  # Command has usage attribute set
+                        embed.add_field(name=f'**{prefix}{cmd.usage}**', value=f'_{cmd.brief}_', inline=True)
+                    else:
+                        embed.add_field(name=f'**{prefix}{cmd.name}**', value=f'_{cmd.brief}_', inline=True)
+
+        return embed
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """ Set presence to let users know the help command. """
+        activity = discord.Activity(type=discord.ActivityType.listening, name="I am a simple discord bot")
+        await self.bot.change_presence(activity=activity)
+
+    async def cog_before_invoke(self, ctx):
+        """ Trigger typing at the start of every command. """
+        await ctx.trigger_typing()
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        """ Send help message when a mis-entered command is received. """
+
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        if type(error) is commands.CommandNotFound:
+            # Get Levenshtein distance from commands
+            in_cmd = ctx.invoked_with
+            bot_cmds = list(self.bot.commands)
+            lev_dists = [lev.distance(in_cmd, str(cmd)) / max(len(in_cmd), len(str(cmd))) for cmd in bot_cmds]
+            lev_min = min(lev_dists)
+
+            # Prep help message title
+            embed_title = f'**```{ctx.message.content}```** is not valid!'
+            prefix = self.bot.command_prefix
+            prefix = prefix[0] if prefix is not str else prefix
+
+            # Make suggestion if lowest Levenshtein distance is under threshold
+            if lev_min <= 0.5:
+                embed_title += f' Did you mean `{prefix}{bot_cmds[lev_dists.index(lev_min)]}`?'
+            else:
+                embed_title += f' Use `{prefix}help` for a list of commands'
+
+            embed = discord.Embed(title=embed_title)
+            await ctx.send(embed=embed)
+
+    @commands.command(brief='Display the help menu')  # TODO: Add 'or details of the specified command'
+    async def help(self, ctx):
+        """ Generate and send help embed based on the bot's commands. """
+        embed = self.help_embed('__Simple Bot Commands__')
+        await ctx.send(embed=embed, delete_after=30)
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
